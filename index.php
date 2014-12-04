fe3513982bcd56e109dd972fb00824ea9e1d590a
mzainularifin <inuld4ta06@gmail.com>
2014-12-04 10:07:21 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Kesiswaan Yayasan Matholi'ul Anwar Simo Sungelebak Karanggeneng Lamongan</title>
+    </head>
+    <body>
+        Hello World
+    </body>
+</html>
